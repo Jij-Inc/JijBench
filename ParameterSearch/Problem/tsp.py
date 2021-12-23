b8dcc140733ed1e328a7c0436dbfcbaeeb85b14e
Hidestament <h.shinohara@j-ij.com>
2021-12-23 20:35:06 +0900

    add tsp

--- ParameterSearch/Problem/tsp.py
+++
@@ -1,7 +0,0 @@
-import jijmodeling as jm
-
-
-def travelling_salesman_problem():
-    # 問題
-    problem = jm.Problem('tsp')
-    return problem
---
+++ parasearch/problems/__init__.py
@@ -0,0 +1,4 @@
+from .bin_packing import bin_packing
+from .strip_packing import strip_packing
+
+__all__ = ["bin_packing", "strip_packing"]
