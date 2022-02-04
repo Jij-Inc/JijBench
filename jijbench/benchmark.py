501c7c1c033c9f283e251c398441aac6f7359d5a
Ubuntu <azureuser@parasearch-bench.hxsbpgcgjoeejipq0rgglb5eih.lx.internal.cloudapp.net>
2022-02-04 00:26:35 +0000

    refactor

--- jijbench/benchmark.py
+++ jijbench/benchmark.py
@@ -1,3 +1,4 @@
+from abc import ABCMeta
 from typing import Callable, List, Dict, Any
 import glob
 import jijmodeling as jm
@@ -5,7 +6,7 @@ from jijmodeling.transpilers.type_annotations import PH_VALUES_INTERFACE
 import dimod
 import json
 from experiment import Experiment
-from parasearch import problems
+from jijbench import problems
 
 
 class Benchmark:
@@ -15,8 +16,9 @@ class Benchmark:
         sampler: Callable[
             [jm.Problem, PH_VALUES_INTERFACE, Dict[str, float], Any], dimod.SampleSet
         ],
-        target_instances="all",
+        target_problems="all",
         n_instances_per_problem="all",
+        instance_files=None,
         optional_args=None,
         instance_dir="./Instances",
         result_dir="./Results",
@@ -26,30 +28,22 @@ class Benchmark:
         Args:
             updater (Callable[[jm.Problem, jm.DecodedSamples, Dict], Dict]): parameter update function
             sampler (Callable[[jm.Problem, PH_VALUES_INTERFACE, Dict[str, float], Any], dimod.SampleSet]): sampler method.
-            target_instances (str, optional): target instance name. Defaults to "all".
+            target_problems (str, optional): target instance name. Defaults to "all".
             n_instances_per_problem (str, optional): number of instance. Defaults to "all".
-            optional_args (dict, optional): [description]. Defaults to {}.
+            optional_args (dict, optional): [description]. Defaults to None.
             instance_dir (str, optional): [description]. Defaults to "./Instances".
             result_dir (str, optional): [description]. Defaults to "./Results".
         """
         self.updater = updater
         self.sampler = sampler
-        self.target_instances = target_instances
-        self.n_instances_per_problem = n_instances_per_problem
-        self.instance_dir = instance_dir
+        self.instance_context = _InstanceContext(
+            target_problems, n_instances_per_problem, instance_files, instance_dir
+        )
         self.result_dir = result_dir
         self.optional_args = optional_args
 
         self._problems = {}
         self._experiments = []
-
-    @property
-    def problems(self):
-        return self._problems
-
-    @problems.setter
-    def problems(self, problems: Dict[str, jm.Problem]):
-        self._problems = problems
 
     @property
     def experiments(self):
@@ -60,34 +54,32 @@ class Benchmark:
         self._experiments = experiments
 
     def setup(self):
-        if self.target_instances == "all":
+        """if self.target_instances == "all":
             for name in problems.__all__:
                 self.problems[name] = getattr(problems, name)()
         else:
             for name in self.target_instances:
-                self.problems[name] = getattr(problems, name)()
+                self.problems[name] = getattr(problems, name)()"""
 
-        for name, problem in self.problems.items():
-            instance_files = glob.glob(
-                f"{self.instance_dir}/{name}/**/*.json", recursive=True
+        for instance_file in self.instance_context.files():
+            experiment = Experiment(
+                self.updater,
+                self.sampler,
+                result_dir=self.result_dir,
+                optional_args=self.optional_args,
             )
-            if isinstance(self.n_instances_per_problem, int):
-                instance_files = instance_files[: self.n_instances_per_problem]
-            for instance_file in instance_files:
-                experiment = Experiment(self.updater, result_dir=self.result_dir)
-                experiment.setting.updater = self.updater.__name__
-                experiment.setting.problem_name = name
-                instance_name = instance_file.lstrip(self.instance_dir).rstrip(".json")
-                experiment.setting.instance_name = instance_name
-                experiment.setting.mathmatical_model = (
-                    jm.expression.serializable.to_serializable(problem)
-                )
-                with open(instance_file, "rb") as f:
-                    experiment.setting.ph_value = json.load(f)
-                experiment.setting.opt_value = experiment.setting.ph_value.pop(
-                    "opt_value", None
-                )
-                self._experiments.append(experiment)
+            # experiment.setting.problem_name = name
+            # instance_name = instance_file.lstrip(self.instance_dir).rstrip(".json")
+            experiment.setting.instance_file = instance_file
+            # experiment.setting.mathmatical_model = (
+            #    jm.expression.serializable.to_serializable(problem)
+            # )
+            # with open(instance_file, "rb") as f:
+            #    experiment.setting.ph_value = json.load(f)
+            #experiment.setting.opt_value = experiment.setting.ph_value.pop(
+            #    "opt_value", None
+            #)
+            self._experiments.append(experiment)
 
     def run(self, sampling_params={}, max_iters=10):
         """run experiment
@@ -108,24 +100,79 @@ class Benchmark:
         #     print()
 
 
+class _InstanceState(metaclass=ABCMeta):
+    def files():
+        pass
+
+
+class _SpecificInstance(_InstanceState):
+    def __init__(self, instance_files):
+        self.instance_files = instance_files
+
+    def files(self):
+        return self.instance_files
+
+
+class _AnyInstance(_InstanceState):
+    def __init__(self, target_problems, n_instances, instance_dir):
+        if target_problems == "all":
+            self.target_problems = problems.__all__
+        else:
+            self.target_problems = target_problems
+        self.n_instances = n_instances
+        self.instance_dir = instance_dir
+
+    def files(self):
+        for name in self.target_problems:
+            instance_files = glob.glob(
+                f"{self.instance_dir}/{name}/**/*.json", recursive=True
+            )
+            if isinstance(self.n_instances, int):
+                instance_files.sort()
+                instance_files = instance_files[: self.n_instances]
+        return instance_files
+
+
+class _InstanceContext(_InstanceState):
+    def __init__(self, target_problems, n_instances, instance_files, instance_dir):
+        if instance_files:
+            self.state = _SpecificInstance(instance_files)
+        else:
+            self.state = _AnyInstance(target_problems, n_instances, instance_dir)
+
+    def files(self):
+        return self.state.files()
+
+
 if __name__ == "__main__":
     from users.makino.updater import update_simple
     from users.makino.solver import sample_model
 
-    target_instances = ["knapsack"]
+    target_problems = "all"
 
     instance_size = "small"
+    instance_files = [
+        "/home/azureuser/JijBenchmark/jijbench/Instances/small/knapsack/f1_l-d_kp_10_269.json"
+    ]
+    # instance_files = None
     instance_dir = f"./Instances/{instance_size}"
     result_dir = f"./Results/makino/{instance_size}"
+    result_dir = "./"
+
+    context = _InstanceContext(target_problems, 5, instance_files, instance_dir)
+    files = context.files()
+    print(files)
 
     bench = Benchmark(
         update_simple,
         sample_model,
-        target_instances=target_instances,
+        target_problems=target_problems,
         n_instances_per_problem=1,
+        instance_files=instance_files,
         instance_dir=instance_dir,
         result_dir=result_dir,
     )
-    bench.run(max_iters=2)
+    bench.run(max_iters=1)
+    fafafaf
 
     print(bench.experiments)
--- jijbench/experiment.py
+++ jijbench/experiment.py
@@ -1,6 +1,8 @@
+from abc import ABCMeta, abstractmethod
 from dataclasses import dataclass, asdict
 from typing import Dict, Callable, Any
-from parasearch import problems
+
+from jijbench import problems
 import datetime
 import os
 import json
@@ -12,8 +14,8 @@ class ExperimentSetting:
     updater: str = ""
     sampler: str = ""
     problem_name: str = ""
-    instance_name: str = ""
     mathmatical_model: dict = None
+    instance_file: str = ""
     ph_value: dict = None
     opt_value: float = -1.0
     multipliers: dict = None
@@ -37,10 +39,17 @@ class Experiment:
     ) -> None:
         self.updater = updater
         self.sampler = sampler
+        self.result_dir = result_dir
+        if optional_args:
+            self.optional_args = optional_args
+        else:
+            self.optional_args = {}
+
         self.setting = ExperimentSetting(
-            updater=updater.__name__, sampler=sampler.__name__
+            updater=self.updater.__name__, sampler=self.sampler.__name__
         )
         self.results = ExperimentResult()
+
         self.datetime = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
         self.result_dir = result_dir
         self.log_dir = f"{result_dir}/{self.datetime}/logs"
@@ -48,17 +57,24 @@ class Experiment:
         self.table_dir = f"{result_dir}/{self.datetime}/tables"
 
     def run(self, problem: jm.Problem, ph_value: Dict, max_iters=10):
-        def _initialize_multipliers(problem: jm.Problem):
+        def _initialize_multipliers():
             _multipliers = {}
             for _key in problem.constraints.keys():
                 _multipliers[_key] = 1
             return _multipliers
-        
-        problem = self.setting.problem_name
-        ph_value = self.setting.ph_value
-        multipliers = _initialize_multipliers(problem)
-        
+
+        self.setting.problem_name = problem.name
+        self.setting.mathmatical_model = jm.expression.serializable.to_serializable(
+            problem
+        )
+        self.setting.ph_value = ph_value
+        self.setting.opt_value = ph_value.pop("opt_value", None)
         self.setting.multipliers = {}
+
+        self.results.penalties = {}
+        self.results.raw_response = {}
+
+        multipliers = _initialize_multipliers()
         # 一旦デフォルトのnum_reads, num_sweepsでupdaterを動かす
         for step in range(max_iters):
             self.setting.multipliers[step] = multipliers
@@ -76,8 +92,32 @@ class Experiment:
             penalties = []
             for violations in decoded.constraint_violations:
                 penalties.append(sum(value for value in violations.values()))
-            self.results["penalties"][step] = penalties
-            self.results["raw_response"][step] = response.to_serializable()
+            self.results.penalties[step] = penalties
+            self.results.raw_response[step] = response.to_serializable()
+
+    def load(self, filename: str) -> "Experiment":
+        """load date
+        saveで保存した結果をそのままloadする.
+        Returns:
+            Experiment: loaded Experiment object.
+        """
+        with open(filename, "r") as f:
+            data = json.load(f)
+
+        date = data["date"]
+        setting = data["setting"]
+        results = data["results"]
+
+        updater = self.updater
+        sampler = self.sampler
+        optional_args = self.optional_args
+        result_dir = self.result_dir
+
+        obj = Experiment(updater, sampler, result_dir, optional_args)
+        obj.datetime = date
+        obj.setting = ExperimentSetting(**setting)
+        obj.results = ExperimentResult(**results)
+        return obj
 
     def save(self):
         instance_name = self.setting.instance_name
@@ -93,28 +133,23 @@ class Experiment:
         with open(filename, "w") as f:
             json.dump(save_obj, f)
 
-    @classmethod
-    def load(cls, filename: str) -> "Experiment":
-        """load date
-        saveで保存した結果をそのままloadする.
-        Returns:
-            Experiment: loaded Experiment object.
-        """
-        with open(filename, "r") as f:
-            data = json.load(f)
-
-        date = data["date"]
-        setting = data["setting"]
-        results = data["results"]
-
-        obj = Experiment()
-        obj.datetime = date
-        obj.setting = ExperimentSetting(**setting)
-        obj.results = ExperimentResult(**results)
-
-        return obj
-
 
 if __name__ == "__main__":
-    setting = ExperimentSetting()
-    print(setting)
+    from users.makino.updater import update_simple
+    from users.makino.solver import sample_model
+
+    problem = problems.knapsack()
+    instance_file = "/home/azureuser/JijBenchmark/jijbench/Instances/small/knapsack/f1_l-d_kp_10_269.json"
+    with open(instance_file, "r") as f:
+        ph_value = json.load(f)
+
+    experiment = Experiment(
+        updater=update_simple, sampler=sample_model, result_dir="./"
+    )
+    experiment.run(problem, ph_value, max_iters=1)
+    experiment.save()
+
+    experiment = experiment.load(
+        "/home/azureuser/JijBenchmark/jijbench/20220203_213241/logs/log.json"
+    )
+    print(experiment.setting)
