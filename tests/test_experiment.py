7f3537a7240630f4b7c5dfd5385b0d1007ba7cfa
yuyamashiro <yuyamashiro89@gmail.com>
2022-03-04 10:27:25 +0000

    fix reconstruct record bugs

--- tests/test_experiment.py
+++ tests/test_experiment.py
@@ -1,4 +1,6 @@
 import jijbench as jb
+import openjij as oj
+import jijmodeling as jm
 
 
 def test_run_id():
@@ -39,5 +41,51 @@ def test_store():
 
     assert len(experiment.table['run_id'].unique()) == row_num
     assert len(experiment.table['experiment_id'].unique()) == 1 
+
+
+def test_openjij():
+    sampler = oj.SASampler()
+    experiment = jb.Experiment(autosave=False)
+
+    with experiment.start():
+        response = sampler.sample_qubo({(0, 1): 1})
+        experiment.store({"result": response})
+    
+
+    droped_table = experiment.table.dropna(axis='columns')
+
+    cols = droped_table.columns
+    "energy" in cols
+    "energy_min" in cols
+
+
+def test_jijmodeling():
+    d = jm.Placeholder("d")
+    x = jm.Binary("x", shape=(2,))
+    problem = jm.Problem("sample")
+    problem += x[0] + d*x[1]
+
+    ph_value = {"d": 2}
+    pyq_obj = problem.to_pyqubo(ph_value=ph_value)
+    pyq_model = pyq_obj.compile()
+
+    sampler = oj.SASampler()
+    experiment = jb.Experiment(autosave=False)
+
+    with experiment.start():
+        bqm = pyq_model.to_bqm()
+        response = sampler.sample(bqm)
+        decoded = problem.decode(response, ph_value=ph_value)
+        experiment.store({"result": decoded})
+    
+
+    droped_table = experiment.table.dropna(axis='columns')
+
+    cols = droped_table.columns
+    "energy" in cols
+    "energy_min" in cols
+    "num_feasible" in cols
+
+
 
 
