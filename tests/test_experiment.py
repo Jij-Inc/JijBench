1813f7f2eac4d32f6c6881a9e1f80d069706d9f2
yuyamashiro <yuyamashiro89@gmail.com>
2022-03-07 01:09:28 +0000

    add test

--- tests/test_experiment.py
+++ tests/test_experiment.py
@@ -1,6 +1,18 @@
+import os
+import shutil
 import jijbench as jb
 import openjij as oj
+import pytest
 import jijmodeling as jm
+
+
+@pytest.fixture(scope="function", autouse=True)
+def pre_post_process():
+    # preprocess
+    yield
+    # postprocess
+    if os.path.exists("./.jb_results"):
+        shutil.rmtree("./.jb_results")
 
 
 def test_run_id():
@@ -120,5 +132,48 @@ def test_file_save_load():
     assert len(experiment.artifact) == len(load_experiment.artifact)
     for artifact in load_experiment.artifact.values():
         assert isinstance(artifact["result"], jm.DecodedSamples)
+    
+    assert experiment._artifact_time_stamp == load_experiment._artifact_time_stamp
+
+def test_auto_save():
+    experiment = jb.Experiment(autosave=True)
+    sampler = oj.SASampler()
+    num_rows = 3
+    for row in range(num_rows):
+        with experiment.start():
+            response = sampler.sample_qubo({(0, 1): 1})
+            experiment.store({"result": response})
+        assert os.path.exists(experiment._dirs.artifact_dir + f"/{experiment.run_id}/time_stamp.txt")
+        load_experiment = jb.Experiment.load(experiment_id=experiment.experiment_id, benchmark_id=experiment.benchmark_id)
+        assert len(load_experiment.table) == row + 1
+
+
+def test_store_same_time_stamp():
+    d = jm.Placeholder("d")
+    x = jm.Binary("x", shape=(2,))
+    problem = jm.Problem("sample")
+    problem += x[0] + d*x[1]
+
+    ph_value = {"d": 2}
+    pyq_obj = problem.to_pyqubo(ph_value=ph_value)
+    pyq_model = pyq_obj.compile()
+
+    sampler = oj.SASampler()
+    experiment = jb.Experiment(autosave=False)
+
+    for _ in range(3):
+        with experiment.start():
+            bqm = pyq_model.to_bqm()
+            response = sampler.sample(bqm)
+            decoded = problem.decode(response, ph_value=ph_value)
+            experiment.store({"result": decoded})
+
+    run_id = list(experiment.artifact.keys())[0]
+
+    artifact_timestamp = experiment._artifact_time_stamp[run_id]
+    table_timestamp = experiment.table[experiment.table["run_id"] == run_id]["time_stamp"][0]
+
+    assert artifact_timestamp == table_timestamp
+
 
     
