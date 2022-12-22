11779ada3e7accbf65b1bb13689b8cd8a0f4bba0
tatsuyagoto <t.goto@j-ij.com>
2022-12-22 07:54:45 +0000

    make get_experiment_id_list function for load

--- tests/test_benchmark.py
+++ tests/test_benchmark.py
@@ -425,6 +425,22 @@ def test_load_invalid_experiment_id():
         bench = jb.load(benchmark_id="test", experiment_id=INVALID_EXPERIMENT_ID)
 
 
+def test_get_experiment_id_list():
+    from jijbench.benchmark.benchmark import get_experiment_id_list
+    from jijbench.components import ExperimentResultDefaultDir
+
+    def func1(x):
+        return 2 * x
+
+    bench = jb.Benchmark(params={"x": [1, 2, 3]}, solver=func1, benchmark_id="test")
+    bench.run()
+    experiment_id_list = list(bench.table["experiment_id"].unique())
+
+    experiment_id_list_load = get_experiment_id_list("test", ExperimentResultDefaultDir)
+
+    assert sorted(experiment_id_list) == sorted(experiment_id_list_load)
+
+
 def test_save():
     def func1(x):
         return 2 * x
