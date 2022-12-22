b15fe4bb3d411d9404ce48119196bacadde1921a
tatsuyagoto <t.goto@j-ij.com>
2022-12-22 07:33:41 +0000

    minor change in test_components.py

--- tests/test_components.py
+++ tests/test_components.py
@@ -15,10 +15,8 @@ def test_load_dtypes_invalid_id():
     bench = jb.Benchmark(params={"x": [1, 2, 3]}, solver=func1, benchmark_id="test")
     bench.run()
 
-    experiment_id = bench.table["experiment_id"].values[0]
-
     with pytest.raises(LoadFailedError):
-        dtypes = Table.load_dtypes(
+        Table.load_dtypes(
             benchmark_id=INVALID_BENCHMARK_ID,
             experiment_id=INVALID_EXPERIMENT_ID,
             autosave=True,
