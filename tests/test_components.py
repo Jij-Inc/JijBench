edda9a1712c7e860715b2db13d1c77380449feed
tatsuyagoto <t.goto@j-ij.com>
2022-12-26 08:48:50 +0000

    add test for benchmark/validation.py

--- tests/test_components.py
+++ tests/test_components.py
@@ -5,7 +5,26 @@ from jijbench.components import ExperimentResultDefaultDir, Table
 from jijbench.exceptions import LoadFailedError
 
 
-def test_load_dtypes_invalid_id():
+def test_table_load_invalid_id():
+    INVALID_BENCHMARK_ID = "invalid_benchmark_id"
+    INVALID_EXPERIMENT_ID = "invalid_experiment_id"
+
+    def func1(x):
+        return 2 * x
+
+    bench = jb.Benchmark(params={"x": [1, 2, 3]}, solver=func1, benchmark_id="test")
+    bench.run()
+
+    with pytest.raises(LoadFailedError):
+        Table.load(
+            benchmark_id=INVALID_BENCHMARK_ID,
+            experiment_id=INVALID_EXPERIMENT_ID,
+            autosave=True,
+            save_dir=ExperimentResultDefaultDir,
+        )
+
+
+def test_table_load_dtypes_invalid_id():
     INVALID_BENCHMARK_ID = "invalid_benchmark_id"
     INVALID_EXPERIMENT_ID = "invalid_experiment_id"
 
