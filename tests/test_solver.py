d3284f5041f7e19d163da4f97a7613c66a159c93
Ubuntu <d@makino-dev.hxsbpgcgjoeejipq0rgglb5eih.lx.internal.cloudapp.net>
2023-02-09 14:20:22 +0000

    add instance_data and user defined model

--- tests/test_solver.py
+++ tests/test_solver.py
@@ -1,6 +1,10 @@
 import pytest
 
 import jijbench as jb
+import jijmodeling as jm
+import numpy as np
+import pytest
+
 from jijbench.exceptions.exceptions import SolverFailedError
 
 
@@ -27,3 +31,59 @@ def test_CallebleSolver_solver_failed_error():
     solver = jb.Solver(custom_solver_failed)
     with pytest.raises(SolverFailedError):
         solver([])
+
+
+def test_instance_data():
+    instance_data = jb.InstanceData(
+        {
+            "int": 1,
+            "float": 1.0,
+            "list": [1, 2.0],
+            "ndarray": np.array([[1, 2], [3, 4]]),
+        },
+        "sample"
+    )
+    
+    assert instance_data.data["int"] == 1
+    assert instance_data.data["float"] == 1.0
+    assert instance_data.data["list"] == [1, 2.0]
+    assert isinstance(instance_data.data["ndarray"], np.ndarray)
+
+def test_invalid_instance_data_keys():
+    with pytest.raises(TypeError):
+        instance_data = jb.InstanceData(
+            {
+                "int": 1,
+                "float": 1.0,
+                0: [1, 2.0],
+                1: np.array([[1, 2], [3, 4]]),
+            },
+            "sample"
+        )
+    
+def test_invalid_instance_data_values():
+    with pytest.raises(TypeError):
+        instance_data = jb.InstanceData(
+            {
+                "int": "a",
+                "float": {"b": 1},
+                "list": [1, 2.0],
+                "ndarray": np.array([[1, 2], [3, 4]]),
+            },
+            "sample"
+        )
+
+def test_user_defined_model():
+    problem = jm.Problem("sample")
+    a = jm.Placeholder("a")
+    b = jm.Placeholder("b")
+    c = jm.Placeholder("c", 1)
+    i = jm.Element("i", 5)
+    x = jm.DecisionVariable("x", 5)
+    
+    problem = jm.Problem("sample")
+    problem += a + b + jm.Sum(i, c[i] * x[i])
+    problem
+    
+    
+    
