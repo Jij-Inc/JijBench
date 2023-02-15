7387fc688b1640b494d99ac6ae84a2f8ccec6c93
tatsuyagoto <t.goto@j-ij.com>
2023-02-15 09:47:44 +0000

    develop metrics plot (on going)

--- jijbench/visualize/metrics/utils.py
+++ jijbench/visualize/metrics/utils.py
@@ -1,4 +1,7 @@
+from __future__ import annotations
+
 import jijmodeling as jm
+import pandas as pd
 
 from jijbench.experiment.experiment import Experiment
 from jijbench.solver.base import Return
@@ -12,3 +15,33 @@ def construct_experiment_from_sampleset(sampleset: jm.SampleSet) -> Experiment:
     record = factory(ret)
     experiment.append(record)
     return experiment
+
+
+def create_fig_title_list(
+    metrics: pd.Series,
+    title: str | list[str] | None,
+):
+    if isinstance(title, list):
+        title_list = title
+        return title_list
+    elif isinstance(title, str):
+        title_list = [title for _ in range(len(metrics))]
+        return title_list
+    elif title is None:
+        title_list = []
+        index_names = metrics.index.names
+        for indices, data in metrics.items():
+            if indices is None:
+                title_list.append("")
+            else:
+                title_list.append(
+                    "\n".join(
+                        [
+                            f"{index_name}: {index}"
+                            for index_name, index in zip(index_names, indices)
+                        ]
+                    )
+                )
+        return title_list
+    else:
+        raise TypeError("title must be str or list[str].")
--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -1,4 +1,5 @@
 import jijmodeling as jm
+import matplotlib
 import numpy as np
 import pandas as pd
 import pytest
@@ -6,7 +7,10 @@ import pytest
 import jijbench as jb
 
 from jijbench.visualize.metrics.plot import get_violations_dict, MetricsPlot
-from jijbench.visualize.metrics.utils import construct_experiment_from_sampleset
+from jijbench.visualize.metrics.utils import (
+    construct_experiment_from_sampleset,
+    create_fig_title_list,
+)
 
 
 def solve():
@@ -49,6 +53,10 @@ def solve():
     return jm_sampleset
 
 
+def simple_func_for_boxplot(x: pd.Series) -> dict:
+    return {"data1": np.array([1, 2, 3, 4]), "data2": np.array([1, 2, 3, 4])}
+
+
 def test_utils_construct_experiment_from_sampleset():
     sampleset = jm.SampleSet(
         record=jm.Record(
@@ -70,6 +78,59 @@ def test_utils_construct_experiment_from_sampleset():
     assert experiment.table["num_occurrences"].values == [np.array([4])]
 
 
+params = {
+    "input is title list": (["title1", "title2"], ["title1", "title2"]),
+    "input is title string": ("title", ["title", "title"]),
+    "input is None": (None, ["i: 1", "i: 2"]),
+}
+
+
+@pytest.mark.parametrize(
+    "input_title, expect",
+    list(params.values()),
+    ids=list(params.keys()),
+)
+def test_utils_create_fig_title_list(input_title, expect):
+    series = pd.Series(
+        data=[1, 2],
+        index=["1", "2"],
+    )
+    series.index.names = ["i"]
+
+    title_list = create_fig_title_list(
+        metrics=series,
+        title=input_title,
+    )
+    assert title_list == expect
+
+
+def test_utils_create_fig_title_list_for_series_with_no_index():
+    series = pd.Series(
+        data=[1, 2],
+        index=[None, None],
+    )
+    title_list = create_fig_title_list(
+        metrics=series,
+        title=None,
+    )
+    assert title_list == ["", ""]
+
+
+def test_utils_create_fig_title_list_for_invalid_input():
+    invalid_input_title = 0
+
+    series = pd.Series(
+        data=[1, 2],
+        index=["1", "2"],
+    )
+    series.index.names = ["i"]
+    with pytest.raises(TypeError):
+        create_fig_title_list(
+            metrics=series,
+            title=invalid_input_title,
+        )
+
+
 def test_metrics_plot_get_violations_dict():
     series = pd.Series(
         data=[np.array([1, 1]), np.array([0, 2])],
@@ -81,15 +142,82 @@ def test_metrics_plot_get_violations_dict():
     assert (violations_dict["onehot_violations"] == np.array([0, 2])).all()
 
 
-def test_metrics_plot_boxplot_violations():
-    expect = 2
+def test_metrics_plot_boxplot():
+    num_multipliers = 2
+    params = {"multipliers": [{} for _ in range(num_multipliers)]}
+    solver_list = [solve]
 
-    multipliers = [{} for _ in range(expect)]
+    expect_num_fig = len(params["multipliers"]) * len(solver_list)
+
     bench = jb.Benchmark(
-        params={"multipliers": multipliers},
-        solver=solve,
+        params=params,
+        solver=solver_list,
     )
     result = bench()
     mplot = MetricsPlot(result)
-    fig_ax_tuple = mplot.boxplot_violations()
-    assert len(fig_ax_tuple) == expect
+    fig_ax_tuple = mplot.boxplot(f=simple_func_for_boxplot)
+    assert len(fig_ax_tuple) == expect_num_fig
+    assert type(fig_ax_tuple[0][0]) == matplotlib.figure.Figure
+    assert type(fig_ax_tuple[0][1]) == matplotlib.axes.Subplot
+
+
+def test_metrics_plot_boxplot_call_maplotlib_boxplot(mocker):
+    m = mocker.patch("matplotlib.axes.Subplot.boxplot")
+
+    num_multipliers = 2
+    params = {"multipliers": [{} for _ in range(num_multipliers)]}
+    solver_list = [solve]
+
+    expect_num_call = len(params["multipliers"]) * len(solver_list)
+
+    bench = jb.Benchmark(
+        params=params,
+        solver=solver_list,
+    )
+    result = bench()
+    mplot = MetricsPlot(result)
+    mplot.boxplot(f=simple_func_for_boxplot)
+
+    assert m.call_count == expect_num_call
+
+
+def test_metrics_plot_boxplot_arg_figsize():
+    figwidth, figheight = 8, 4
+
+    bench = jb.Benchmark(
+        params={},
+        solver=[solve],
+    )
+    result = bench()
+    mplot = MetricsPlot(result)
+    fig_ax_tuple = mplot.boxplot(
+        f=simple_func_for_boxplot,
+        figsize=(figwidth, figheight),
+    )
+    fig, ax = fig_ax_tuple[0]
+
+    assert fig.get_figwidth() == 8
+    assert fig.get_figheight() == 4
+
+
+def test_metrics_plot_boxplot_arg_title(mocker):
+    m = mocker.patch("matplotlib.figure.Figure.suptitle")
+
+    title = ["title1", "title2"]
+
+    num_multipliers = 2
+    params = {"multipliers": [{} for _ in range(num_multipliers)]}
+    solver_list = [solve]
+
+    bench = jb.Benchmark(
+        params=params,
+        solver=solver_list,
+    )
+    result = bench()
+    mplot = MetricsPlot(result)
+    mplot.boxplot(
+        f=simple_func_for_boxplot,
+        title=title,
+    )
+    m.assert_any_call("title1")
+    m.assert_any_call("title2")
